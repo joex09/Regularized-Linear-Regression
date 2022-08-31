49bcf36c8a590c371ae1c5cea252e3e38829693f
joex09 <a.suarez.mila@gmail.com>
2022-08-31 19:28:15 +0000

    trabajo terminado

--- src/app.py
+++ src/app.py
@@ -1,4 +1,162 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt  
+import seaborn as sns
+import plotly.express as px
+from sklearn.model_selection import train_test_split, cross_validate
+import statsmodels.api as sm
+from sklearn import metrics
+from sklearn.model_selection import GridSearchCV
+import pickle
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import Lasso
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LassoCV
 
-# your code here
+df = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/regularized-linear-regression-project-tutorial/main/dataset.csv')
+
+
+x = df.drop(['Heart disease_prevalence', 'Heart disease_Lower 95% CI',
+       'Heart disease_Upper 95% CI', 'COPD_prevalence', 'COPD_Lower 95% CI',
+       'COPD_Upper 95% CI', 'diabetes_prevalence', 'diabetes_Lower 95% CI',
+       'diabetes_Upper 95% CI', 'CKD_prevalence', 'CKD_Lower 95% CI',
+       'CKD_Upper 95% CI'],axis=1)
+y = df['Heart disease_prevalence']
+
+
+x = x.drop(['COUNTY_NAME'], axis=1)
+x = pd.get_dummies(x,drop_first=True) 
+X_train, X_test, y_train, y_test = train_test_split(x, y, random_state=53, test_size=0.15)
+
+modelo = Lasso(alpha = 0.3,normalize = True)
+modelo.fit(X_train,y_train)
+
+predicciones = modelo.predict(X_test)
+rmse_lasso = mean_squared_error(
+y_true = y_test,
+y_pred = predicciones,
+squared = False
+)
+print("")
+print(f"El error (rmse) de test es: {rmse_lasso}")
+
+modelo = LassoCV(
+alphas = np.logspace(-10, 3, 200),
+normalize = True,
+cv = 10
+)
+_ = modelo.fit(X = X_train, y = y_train)
+
+
+alphas = modelo.alphas_
+coefs = []
+
+for alpha in alphas:
+    modelo_temp = Lasso(alpha=alpha, fit_intercept=False, normalize=True)
+    modelo_temp.fit(X_train, y_train)
+    coefs.append(modelo_temp.coef_.flatten())
+
+fig, ax = plt.subplots(figsize=(7, 3.84))
+ax.plot(alphas, coefs)
+ax.set_xscale('log')
+ax.set_ylim([-15,None])
+ax.set_xlabel('alpha')
+ax.set_ylabel('coeficientes')
+ax.set_title('Coeficientes del modelo en función de la regularización')
+
+
+alphas = modelo.alphas_
+n_predictores = []
+
+for alpha in alphas:
+    modelo_temp = Lasso(alpha=alpha, fit_intercept=False, normalize=True)
+    modelo_temp.fit(X_train, y_train)
+    coef_no_cero = np.sum(modelo_temp.coef_.flatten() != 0)
+    n_predictores.append(coef_no_cero)
+
+fig, ax = plt.subplots(figsize=(7, 3.84))
+ax.plot(alphas, n_predictores)
+ax.set_xscale('log')
+ax.set_ylim([-15,None])
+ax.set_xlabel('alpha')
+ax.set_ylabel('nº predictores')
+ax.set_title('Predictores incluidos en función de la regularización');
+
+
+mse_cv = modelo.mse_path_.mean(axis=1)
+mse_sd = modelo.mse_path_.std(axis=1)
+
+# Se aplica la raíz cuadrada para pasar de mse a rmse
+rmse_cv = np.sqrt(mse_cv)
+rmse_sd = np.sqrt(mse_sd)
+
+# Se identifica el óptimo y el óptimo + 1std
+min_rmse = np.min(rmse_cv)
+sd_min_rmse = rmse_sd[np.argmin(rmse_cv)]
+optimo = modelo.alphas_[np.argmin(rmse_cv)]
+
+# Gráfico del error +- 1 desviación estándar
+fig, ax = plt.subplots(figsize=(7, 3.84))
+ax.plot(modelo.alphas_, rmse_cv)
+ax.fill_between(
+modelo.alphas_,
+rmse_cv + rmse_sd,
+rmse_cv - rmse_sd,
+alpha=0.2
+)
+
+ax.axvline(
+x = optimo,
+c = "gray",
+linestyle = '--',
+label = 'óptimo'
+)
+
+
+
+ax.set_xscale('log')
+ax.set_ylim([0,None])
+ax.set_title('Evolución del error CV en función de la regularización')
+ax.set_xlabel('alpha')
+ax.set_ylabel('RMSE')
+plt.legend()
+
+#grafica q mide el error con los dif alfas, vemos el alfa optimo es 
+print(f"Mejor valor de alpha encontrado: {modelo.alpha_}")
+
+# Mejor valor alpha encontrado + 1sd
+min_rmse     = np.min(rmse_cv)
+sd_min_rmse  = rmse_sd[np.argmin(rmse_cv)]
+min_rsme_1sd = np.max(rmse_cv[rmse_cv <= min_rmse + sd_min_rmse])
+optimo       = modelo.alphas_[np.argmin(rmse_cv)]
+optimo_1sd   = modelo.alphas_[rmse_cv == min_rsme_1sd]
+
+print(f"Mejor valor de alpha encontrado + 1 desviación estándar: {optimo_1sd}")
+
+# Mejor modelo alpha óptimo + 1sd
+modelo = Lasso(alpha= 0.00113573, normalize=True)
+modelo.fit(X_train, y_train)
+
+# Coeficientes del modelo
+df_coeficientes = pd.DataFrame(
+                        {'predictor': X_train.columns,
+                         'coef': modelo.coef_.flatten()}
+                  )
+
+# Predictores incluidos en el modelo (coeficiente != 0)
+df_coeficientes[df_coeficientes.coef != 0]
+
+# Predicciones test
+predicciones = modelo.predict(X=X_test)
+predicciones = predicciones.flatten()
+predicciones[:10]
+
+# Error de test del modelo 
+rmse_lasso = mean_squared_error(
+                y_true  = y_test,
+                y_pred  = predicciones,
+                squared = False
+             )
+print("")
+print(f"El error (rmse) de test es: {rmse_lasso}")
